348e27d3af52e808e7d2735c7f978530b2835838
ChubbyQuokka <graybad1@icloud.com>
2018-12-22 00:14:25 -0500

    added catch statements and added eslint

--- src/index.js
+++ src/index.js
@@ -1,41 +1,48 @@
-const discord = require ('discord.js');
-const express = require ('express');
-const bodyParser = require('body-parser');
+const discord = require('discord.js')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const webApp = express();
-const discordClient = new discord.Client();
+const webApp = express()
+const discordClient = new discord.Client()
 
-var token = process.env.TOKEN;
+const token = process.env.TOKEN
+const port = 5000
 
-if (!token){
-    console.log('Please set the enviroment variable \'TOKEN\' to your bot\'s Discord token.');
-    process.exit(1);
+if (!token) {
+  console.log('Please set the enviroment variable \'TOKEN\' to your bot\'s Discord token.')
+  process.exit(1)
 }
 
-webApp.use(bodyParser.json());
+webApp.use(bodyParser.json())
 
 webApp.post('/', async (req, res) => {
-    console.log(req.body);
+  console.log(req.body)
 
-    if (req.body.currentPlayers == undefined || req.body.maxPlayers == undefined){
-        let err = {error: 'Bad request body.'}
-        res.status(400).send(JSON.stringify(err));
-        return;
-    }
+  if (req.body.currentPlayers === undefined || req.body.maxPlayers === undefined) {
+    let err = { error: 'Bad request body.' }
+    res.status(400).send(JSON.stringify(err))
+    return
+  }
 
-    await setDiscordStatus(req.body.currentPlayers, req.body.maxPlayers);
-    res.status(200).send();
-});
+  let statusCode = 200
+
+  await setDiscordStatus(req.body.currentPlayers, req.body.maxPlayers).catch(error => {
+    console.error(error)
+    statusCode = 500
+  })
+
+  res.status(statusCode).send()
+})
 
 discordClient.on('ready', async () => {
-    await setDiscordStatus(0, 0);
-    webApp.listen(5000);
+  await setDiscordStatus(0, 0).catch(console.error)
+  webApp.listen(port)
 
-    console.log('Ready!');
-});
+  console.log('Ready!')
+})
 
-discordClient.login(token);
+discordClient.login(token)
 
-async function setDiscordStatus(currentPlayers, maxPlayers){
-    await discordClient.user.setActivity(`${currentPlayers} / ${maxPlayers} Online Players!`, {type: 'WATCHING'});
+async function setDiscordStatus (currentPlayers, maxPlayers) {
+  await discordClient.user.setActivity(`${currentPlayers} / ${maxPlayers} Online Players!`, { type: 'WATCHING' })
 }
